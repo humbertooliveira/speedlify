3d60be11ba1a45138801c3c63968179bbdf14a56
Humberto Oliveira <humberto@holiveira.com>
2021-06-17 01:47:16 -0300

    Create fuiserviajante.js

---
+++ _data/sites/fuiserviajante.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "Fui ser Viajante", // optional, falls back to object key
+	description: "Home page e post",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.fuiserviajante.com/",
+    "https://www.fuiserviajante.com/bahia/o-que-fazer-em-arraial-dajuda/",
+    "https://www.fuiserviajante.com/planejamento/sul-da-bahia-de-carro/"
+	]
+};
