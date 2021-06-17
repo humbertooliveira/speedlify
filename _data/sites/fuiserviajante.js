06893960bebdc74dbc4ce5eeca750a8f1aaac1ce
Humberto Oliveira <humberto@holiveira.com>
2021-06-17 01:54:29 -0300

    Update fuiserviajante.js

--- _data/sites/fuiserviajante.js
+++ _data/sites/fuiserviajante.js
@@ -1,12 +1,15 @@
 module.exports = {
-	name: "Fui ser Viajante", // optional, falls back to object key
-	description: "Home page e post",
+	name: "fuiserviajante.com", // optional, falls back to object key
+	description: "Fui ser Viajante",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: 60 * 1, // (in minutes), 23 hours
 	},
 	urls: [
 		"https://www.fuiserviajante.com/",
-    "https://www.fuiserviajante.com/bahia/o-que-fazer-em-arraial-dajuda/",
-    "https://www.fuiserviajante.com/planejamento/sul-da-bahia-de-carro/"
+    		"https://www.fuiserviajante.com/bahia/o-que-fazer-em-arraial-dajuda/",
+		"https://www.fuiserviajante.com/planejamento/sul-da-bahia-de-carro/",
+		"https://www.fuiserviajante.com/pagina-inicial/",
+		"https://www.fuiserviajante.com/contato/"
+		
 	]
 };
